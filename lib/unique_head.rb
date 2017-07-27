079d0c5d28d62637c74cce0028bfc5524c1f58e7
Acen <acenqiu@gmail.com>
2017-07-27 17:59:04 +0800

    支持toc锚点

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -6,12 +6,16 @@ class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
     @head_count = {}
   end
   def header(text, header_level)
+    # 使用 || 分隔英文和中文，用于支持toc锚点
+    text, title = text.split(' || ')
+    title ||= text
+
     friendly_text = text.parameterize
     @head_count[friendly_text] ||= 0
     @head_count[friendly_text] += 1
     if @head_count[friendly_text] > 1
       friendly_text += "-#{@head_count[friendly_text]}"
     end
-    return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
+    return "<h#{header_level} id='#{friendly_text}'>#{title}</h#{header_level}>"
   end
 end
